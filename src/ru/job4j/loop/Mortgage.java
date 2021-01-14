895e31a7e435d9201a45d467aa77e3d9af309094
ainz713 <ainz713@gmail.com>
2021-01-14 23:42:11 +0300

    Корректировка по комментариям 5.5. Простое число [#443622] и 5.7. Ипотека [#443621]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,8 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        amount = amount + (amount + amount * (percent / 100));
-        while (salary <= amount) {
+        //amount = amount + (amount + amount * (percent / 100));
+        while (salary * year <= amount) {
+            amount = amount + (amount + amount * (percent / 100));
             amount = amount - salary;
             year++;
         }

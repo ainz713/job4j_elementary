3031a909b06cec9cddc66efe77b6e559d0031932
ainz <ainz713@gmail.com>
2021-01-20 22:57:31 +0300

    6.2. Упорядочить массив. [#443559] Переделал

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -2,9 +2,6 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-//        int temp = array[array.length - 1];
-//        array[array.length - 1] = array[0];
-//        array[0] = temp;
         int temp = 0;
         for (int i = 0; i < array.length / 2; i++) {
         temp = array[array.length - 1 - i];

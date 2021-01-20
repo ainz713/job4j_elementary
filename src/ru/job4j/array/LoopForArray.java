a84b00160f908b89b28b93f4e5600bcbe96c6ebe
ainz <ainz713@gmail.com>
2021-01-21 00:11:24 +0300

    6.4.1.1. Обход массива с последнего элемента [#443556]

---
+++ src/ru/job4j/array/LoopForArray.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class LoopForArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println(numbers[i]);
+            }
+        }
+    }
+}

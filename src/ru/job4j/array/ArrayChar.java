c13367145d66b057be9d03daec7719d9ca632f01
ainz <ainz713@gmail.com>
2021-01-22 00:51:20 +0300

    6.4. Слово начинается с ... [#443531]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -7,6 +7,7 @@ public class ArrayChar {
         for (int i = 0; i <= pref.length - 1; i++) {
             if (word[i] != pref[i]) {
                 result = false;
+                break;
             }
         }
         return result;

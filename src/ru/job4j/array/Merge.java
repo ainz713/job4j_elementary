c9e26de0d61c09dc2bedc8d0b052317830b2cfda
ainz <ainz713@gmail.com>
2021-02-12 13:16:46 +0300

    1. Экзамен. Базовый синтаксис. [#443606] исправил

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -4,35 +4,18 @@ public class Merge {
 
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int temp;
-        for (int i = 0; i < left.length; i++) {
-            for (int j = 0; j < right.length; j++) {
-                if (left[i] > right[j]) {
-                    temp = left[i];
-                    left[i] = right[j];
-                    right[j] = temp;
-                }
+        int leftPoint = 0;
+        int rightPoint = 0;
+        int rslPoint = 0;
+        while (leftPoint + rightPoint != rsl.length) {
+            if (leftPoint != left.length && rightPoint != right.length) {
+                rsl[rslPoint++] = left[leftPoint] < right[rightPoint] ? left[leftPoint++] : right[rightPoint++];
+            } else if (leftPoint != left.length) {
+                rsl[rslPoint++] = left[leftPoint++];
+            } else {
+                rsl[rslPoint++] = right[rightPoint++];
             }
         }
-            for (int j = 0; j < left.length; j++) {
-                rsl[j] = left[j];
-            }
-           for (int k = 0; k < right.length; k++) {
-                rsl[rsl.length - 1 - k] = right[max(right)];
-               right[max(right)] = 0;
-            }
         return rsl;
-    }
-
-    public static int max(int[] left) {
-        int max = left[0];
-        int index = 0;
-        for (int i = 0; i < left.length; i++) {
-            if (max < left[i]) {
-                max = left[i];
-                index = i;
-            }
-        }
-        return index;
     }
 }
